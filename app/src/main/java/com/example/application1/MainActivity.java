187ba4e3f182015d948d39db625dc0365fbf8a4e
hyunsu <cjswkans1224@gmail.com>
2023-09-05 10:34:14 +0900

    FIrebase Login,Logout

---
+++ app/src/main/java/com/example/application1/MainActivity.java
@@ -0,0 +1,42 @@
+package com.example.application1;
+
+import androidx.appcompat.app.AppCompatActivity;
+
+import android.content.Intent;
+import android.os.Bundle;
+import android.view.View;
+import android.widget.Button;
+
+import com.google.firebase.auth.FirebaseAuth;
+import com.google.firebase.auth.FirebaseUser;
+
+public class MainActivity extends AppCompatActivity {
+
+    private FirebaseAuth mFirebaseAuth;
+
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_main);
+
+        mFirebaseAuth = FirebaseAuth.getInstance();
+
+        Button btn_logout = findViewById(R.id.btn_logout);
+        btn_logout.setOnClickListener(new View.OnClickListener() {
+            @Override
+            public void onClick(View view) {
+                // 로그아웃 하기
+                mFirebaseAuth.signOut();
+                // 방법1. 로그아웃 후 로그인 페이지로 이동
+                // Intent intent = new Intent(MainActivity.this, LoginActivity.class);
+                // startActivity(intent);
+                // 방법2. app 종료
+                finish();
+            }
+        });
+
+        // 탈퇴처리
+        // mFirebaseAuth.getCurrentUser().delete();
+    }
+}
